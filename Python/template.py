75c37f8d359a850f8bb96ace2bc82981abb93643
Richard Brunt <r.brunt@gmail.com>
2012-09-12 17:45:16 +0100

    added emtpy python template file

---
+++ Python/template.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+
+
+# Import statements go here
+
+
+# Define functions here:
+
+
+
+def main():
+  # Main program block
+
+
+
+# Starts main...
+if __name__ == '__main__':
+  main()
